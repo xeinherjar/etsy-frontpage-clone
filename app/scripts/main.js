051856ef393f66c023153d5edfe2aff65c595635
Alex Pate <proto.merkabah@gmail.com>
2015-01-21 15:16:36 -0500

    Push items to pages via jQuery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('The Iron Yard Rocks');
+
+var rc = $('.results-content');
+
+function buildIMG(url) {
+  return '<img src="' + url + '" />';
+}
+
+
+etsyItems.results.forEach( function(x) {
+  var img_URL = x.Images[0].url_75x75;
+  var img = '<img src="' + img_URL + '" /"';
+  var name = x.Shop.shop_name;
+  var title = x.title;
+  var url = x.url;
+  var price = x.price;
+
+  var el = '<div class="listing">' +
+           img +
+           '<span>' + title + '</span>' +
+           '<div class="listing-footer">' +
+           '<span>' + name + '</span>' +
+           '<span>' + price + '</span>' +
+           '</div> </div>';
+
+  rc.append(el);
+});
