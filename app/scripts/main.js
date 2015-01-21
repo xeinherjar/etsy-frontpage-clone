bfa736fdd7688e3259449e9659997488d85dbcd8
Alex Pate <proto.merkabah@gmail.com>
2015-01-21 16:28:13 -0500

    listings layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,20 +7,34 @@ function buildIMG(url) {
 
 
 etsyItems.results.forEach( function(x) {
-  var img_URL = x.Images[0].url_75x75;
-  var img = '<img src="' + img_URL + '" /"';
+  var img_URL = x.Images[0].url_170x135;
+  var img = '<img src="' + img_URL + '" />';
   var name = x.Shop.shop_name;
   var title = x.title;
   var url = x.url;
-  var price = x.price;
+
+  var p_sign;
+  switch(x.currency_code) {
+    case "USD":
+      p_sign = "$";
+      break;
+    case "GBP":
+      p_sign = "L";
+      break;
+    default:
+      p_sign: "$";
+
+  }
+  var price = p_sign + x.price + " " + x.currency_code;
 
   var el = '<div class="listing">' +
            img +
-           '<span>' + title + '</span>' +
+           '<div class="title">' + title + '</div>' +
            '<div class="listing-footer">' +
            '<span>' + name + '</span>' +
            '<span>' + price + '</span>' +
-           '</div> </div>';
+           '</div>' +
+           '</div>';
 
   rc.append(el);
 });
