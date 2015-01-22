a8d563a3fc9aa61b90a5959344910eb5d2ec2fec
Alex Pate <proto.merkabah@gmail.com>
2015-01-22 14:11:34 -0500

    final touch ups

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,11 @@
 
 var rc = $('.results-content');
 
-function buildIMG(url) {
-  return '<img src="' + url + '" />';
-}
-
 
 etsyItems.results.forEach( function(x) {
   var img_URL = x.Images[0].url_170x135;
-  var img = '<img src="' + img_URL + '" />';
   var name = x.Shop.shop_name;
+  var shop_url = x.Shop.url;
   var title = x.title;
   var url = x.url;
 
@@ -26,12 +22,16 @@ etsyItems.results.forEach( function(x) {
 
   }
   var price = p_sign + x.price + " " + x.currency_code;
-
+  var alt_text = ' title="' + title + '"';
+  var img = '<img src="' + img_URL + '"' + alt_text + '" />';
   var el = '<div class="listing">' +
-           img +
-           '<div class="title">' + title + '</div>' +
+           '<a href="' + url + '">' + img + '</a>' +
+           '<div class="title"' + alt_text +'>' + 
+           '<a href="' + url + '">' + title + '</a></div>' +
            '<div class="listing-footer">' +
-           '<span>' + name + '</span>' +
+           '<span>' + 
+           '<a href="' + shop_url + '" title="' + name + '">' +
+           name + '</a></span>' +
            '<span>' + price + '</span>' +
            '</div>' +
            '</div>';
